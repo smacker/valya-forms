40071b4adbf66e959f6502aa27a2e42e7a612016
Sukharev Maxim <maxim.sukharev@lazada.com>
2016-02-16 12:41:41 +0700

    simplify validator for tests

--- tests/components/validator.js
+++ tests/components/validator.js
@@ -1,26 +1,6 @@
-import React from 'react';
 import Valya from 'valya';
 import { ValidatorWrapper } from '../../src/index';
 
-class Validator extends React.Component {
-    renderError() {
-        const { enabled, isValid } = this.props;
-
-        if (!enabled || isValid) {
-            return null;
-        }
-
-        return <div className="error">{this.props.validationMessage}</div>;
-    }
-
-    render() {
-        return (
-            <div className="validator">
-                <div className="target">{this.props.children}</div>
-                {this.renderError()}
-            </div>
-        );
-    }
-}
-
-export default ValidatorWrapper(Valya(Validator));
+export default ValidatorWrapper(Valya(function(props) {
+    return props.children;
+}));
